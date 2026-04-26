cc4339330b347d32c8a28d56a021c544b407ad05
Prokop Klarovic <prokop.klarovic@skolakrizik.com>
2026-04-26 21:56:11 +0200

    3rd_commit

--- GmHra.cpp
+++ GmHra.cpp
@@ -1,100 +1,302 @@
 #include <iostream>
-#include <ctime>
+#include <conio.h>
 #include <thread>
 #include <chrono>
+
 using namespace std;
+
+void enter() {
+    cout << "\n-";
+    while (_getch() != 13) {}
+    cout << endl;
+}
+
+void getZbran(int x, string &zbran, int &zbranDmg, int &zbranAcc) {
+    switch (x) {
+        case 1://schofield
+            zbran="Schofield Revolver";
+            zbranDmg=0;
+            zbranAcc=1;
+        break;
+        case 2://double barrel
+            zbran="Double Barrel";
+            zbranDmg=1;
+            zbranAcc=0;
+        break;
+        case 3://Lichtfield
+            zbran="Litchfield Repeater";
+            zbranDmg=2;
+            zbranAcc=1;
+        break;
+        case 4://Winchester
+            zbran="Winchester Repeater";
+            zbranDmg=2;
+            zbranAcc=2;
+        break;
+        case 5://schofield
+            zbran="Carabine";
+            zbranDmg=3;
+            zbranAcc=1;
+        break;
+        case 6://Winchester Shotgun
+            zbran="Winchester Shotgun";
+            zbranDmg=1;
+            zbranAcc=0;
+        break;
+        default:
+    cout << "----------------------------" << endl;
+    cout << endl;
+    cout << endl;
+    cout << "Spatny Input: RESTARTUJ HRU!" << endl;
+    cout << endl;
+    cout << endl;
+    cout << "----------------------------" << endl;
+    break;
+    }
+}
+    void getZbran(int x, string &clothes, int &zivot) {
+    switch (x) {
+        case 1://Vesta
+            clothes="Vesta";
+            zivot+=1;
+        break;
+        case 2://Sedlaŕské kalhoty
+            clothes="Sedlařské klahoty";
+            zivot+=1;
+        break;
+        case 3://čepice
+            clothes="Čepice";
+            zivot+=1;
+        break;
+        case 4://Trench Coat
+            clothes="Trench Coat";
+            zivot+=1;
+        break;
+        case 5://plat
+            clothes="Plát pod trikem";
+            zivot+=2;
+        break;
+        default:
+    cout << "----------------------------" << endl;
+    cout << endl;
+    cout << endl;
+    cout << "Spatny Input: RESTARTUJ HRU!" << endl;
+    cout << endl;
+    cout << endl;
+    cout << "----------------------------" << endl;
+    break;
+    }
+}
 
 int main(){
     cout << "                                 $$$$$$ $$$$$ $$  $ $$$$$$      $$$$   $$   $$$$$$ $$$$$ $$$$$                                  " << endl;
     cout << "                                    $   $$     $ $     $        $  $   $$$  $$   $ $      $  $$                                 " << endl;
-    cout << "                                    $   $$$$    $$     $        $$$$   $ $    $$   $$$$  $$  $$                                 " << endl;
-    cout << "                                    $   $$     $$$     $        $  $$ $$ $$ $   $$ $     $$  $$                                 " << endl;
+    cout << "                                    $   $$$$    $$     $        $$$$   $ $    $    $$$$  $$  $$                                 " << endl;
+    cout << "                                    $   $$     $$$     $        $  $$ $$ $$     $$ $     $$  $$                                 " << endl;
     cout << "                                    $   $$     $ $$    $        $   $ $$$$$ $$  $$ $     $$  $$                                 " << endl;
     cout << "                                    $   $$$$$ $@  $$   $        $$$$$ $   $ $$$$$$ $$$$$ $$$$$                                 " << endl;
     cout << "                                                                                                                                " << endl;
     cout << "                       $$$$$$$$$ $$$$$$$$    $$$$$$$$$                                          $$$$$$$$$                       " << endl;
-    cout << "           $$$$$$$$$$  $$$$$$$$$ $$$$$$$$$$  $$$$$$$$$  $$$   $$$  $$$$$$$$$$ $$$$$$$$$$$ $$$$  $$$$$$$$$  $$$$   $$$           " << endl;
-    cout << "           $$$    $$$  $$@         $$$   $$$  $$        $$$$ $$$$$ $$$    $$$     $$$     $$$   $$    $$$  $$$$   $$$           " << endl;
-    cout << "           $$$$$$$$$$  $$          $$$  $$$$  $$        $$$$$$$$$$ $$$$$$@$$$     $$$     $$$   $$@   $$$  $$$$$  $$$           " << endl;
-    cout << "           $$$  $$$    $$$$$       $$$  $$$$  $$$$$     $$$$$$$$$$ $$$$$$$$$$     $$$     $$$$  $$$   $$$  $$$$$$ $$$           " << endl;
-    cout << "           $$$  $$$    $$$$$$$     $$$  $$$$  $$$$$$    $$$$$$$$$$ $$$$           $$$     $$$$  $$$   $$$  $$$ $$$$$$           " << endl;
-    cout << "           $$$  $$$$   $$          $$$  $$$$  $$        $$$ $$ $$$ $$$$           $$$     $$$$  $$$   $$$  $$$  $$@$$           " << endl;
-    cout << "           $$$   $$$   $$          $$$   $$$  $$        $$$ $$ $$$ $$$$           $$$     $$$$  $$$    $$  $$$  $$$$$           " << endl;
-    cout << "           $$$   $$$   $$          $$$   $$$  $$      @ $$$    $$$ $$$$           $$$     $$$$  $$$    $$  $$$   $$$$           " << endl;
-    cout << "           $$$    $$$  $$$$$$$$$  $$$$$$$$$$  $$$$$$$$$ $$$    $$$ $$$$           $$$     $$$$  $$$$$$$$$  $$$    $$$           " << endl;
-    cout << "           $$$    @$$            @$$$$$$$$              $$$    $$  $$$$           $$$     $$$   $$$$$$$$$   $$    $$$           " << endl;
+    cout << "           $$$$$$$$$$  $$$$$$$$$ $$$$$$$$$$  $$$$$$$$$  $$$   $$$   $$$$$$$$$ $$$$$$$$$$$ $$$$  $$$$$$$$$  $$$$   $$$           " << endl;
+    cout << "           $$$    $$$  $$@         $$$   $$$  $$        $$$$ $$$$$  $$$    $$$     $$$     $$$   $$    $$$  $$$$   $$$           " << endl;
+    cout << "           $$$$$$$$$$  $$          $$$  $$$$  $$        $$$$$$$$$$  $$$$@  $$$     $$$     $$$   $$@   $$$  $$$$$  $$$           " << endl;
+    cout << "           $$$  $$$    $$$$$       $$$  $$$$  $$$$$     $$$$$$$$$$  $$$$$$$$$$     $$$     $$$$  $$$   $$$  $$$$$$ $$$           " << endl;
+    cout << "           $$$  $$$    $$$$$$$     $$$  $$$$  $$$$$$    $$$$$$$$$$  $$$$           $$$     $$$$  $$$   $$$  $$$ $$$$$$           " << endl;
+    cout << "           $$$  $$$$   $$          $$$  $$$$  $$        $$$ $$ $$$  $$$$           $$$     $$$$  $$$   $$$  $$$  $$@$$           " << endl;
+    cout << "           $$$   $$$   $$          $$$   $$$  $$        $$$ $$ $$$  $$$$           $$$     $$$$  $$$    $$  $$$  $$$$$           " << endl;
+    cout << "           $$$   $$$   $$          $$$   $$$  $$      @ $$$    $$$  $$$$           $$$     $$$$  $$$    $$  $$$   $$$$           " << endl;
+    cout << "           $$$    $$$  $$$$$$$$$  $$$$$$$$$$  $$$$$$$$$ $$$    $$$  $$$$           $$$     $$$$  $$$$$$$$$  $$$    $$$           " << endl;
+    cout << "           $$$    @$$            @$$$$$$$$              $$$    $$   $$$$           $$$     $$$   $$$$$$$$$   $$    $$$           " << endl;
 int dmg_stat = 0;
 int acc_stat = 0;
 int hp_stat = 0;
 int rng_stat = 0;
+int cash;
 int clas = 1;
-char opakovat = 0;
-string classs[4]={"Gunslinger" ,"Outlaw" ,"Sherif" , "Gold Digger"};
-cout << "[    ACC=presnost   ]" << endl ;
-cout << "[    DMG=poskozeni  ]" << endl ;
-cout << "[     HP=zivoty     ]" << endl ;
-cout << "[     RNG=stesti    ]" << endl ;
-cout << " |ACC|DMG|HP|RNG|" << endl;
+char opakovat = 'n';
+int vyber = 0;
+int zbranDmg = 0;
+int zbranAcc = 0;
+string zbran = "err";
+string classs;
+cout << "[    ACC=přesnost   ]" << endl ;
+cout << "[    DMG=poškození  ]" << endl ;
+cout << "[     HP=životy     ]" << endl ;
+cout << "[     RNG=štěstí    ]" << endl ;
+cout << " $|ACC|DMG|HP|RNG|" << endl;
 cout << "Vyberte si postavu:" << endl;
 cout << "1.       Gunslinger" << endl;
-cout << "          |3|2|2|1|" << endl;
+cout << "      300$|3|2|2|1|" << endl;
 cout << "2.           Outlaw" << endl;
-cout << "          |2|3|2|1|" << endl;
+cout << "      400$|2|3|2|1|" << endl;
 cout << "3.           Sherif" << endl;
-cout << "          |2|2|3|1|" << endl;
+cout << "      350$|2|2|3|1|" << endl;
 cout << "4.      Gold Digger" << endl;
-cout << "          |1|1|1|5|" << endl;
+cout << "      550$|1|1|1|4|" << endl;
 cout << "-------------------" << endl;
 do{
 do{
-    if(clas<0||clas>4){
-    cout << "Zadejte cisla 1,2,3 nebo 4." << endl;
+    if(clas<1||clas>5){
+    cout << "Zadejte čísla 1,2,3 nebo 4." << endl;
     }
+cout << "Výběr:";
 cin>> clas;
 
-}while(clas<0||clas>4);
+}while(clas<1||clas>5);
 switch (clas) {
     case 1:
-        cout << "Vybral si postavu " << classs[clas-1] << "." << endl;
+        classs = "Gunslinger";
+        cout << "Vybral si postavu " << classs << "." << endl;
         acc_stat = 3;
         dmg_stat = 2;
         hp_stat = 2;
         rng_stat = 1;
+        cash = 360;
         break;
     case 2:
-        cout << "Vybral si postavu " << classs[clas-1] << "." << endl;
+        classs = "Outlaw";
+        cout << "Vybral si postavu " << classs << "." << endl;
         acc_stat = 2;
         dmg_stat = 3;
-        hp_stat = 2;
+        hp_stat = 1;
         rng_stat = 1;
+        cash = 420;
         break;
     case 3:
-        cout << "Vybral si postavu " << classs[clas-1] << "." << endl;
+        classs = "Sherif";
+        cout << "Vybral si postavu " << classs << "." << endl;
         acc_stat = 2;
         dmg_stat = 2;
         hp_stat = 3;
         rng_stat = 1;
+        cash = 350;
         break;
     case 4:
-        cout << "Vybral si postavu " << classs[clas-1] << "." << endl;
+        classs = "Gold Digger";
+        cout << "Vybral si postavu " << classs << "." << endl;
         acc_stat = 1;
         dmg_stat = 1;
         hp_stat = 1;
-        rng_stat = 5;
+        rng_stat = 4;
+        cash = 550;
         break;
+    case 5:
+        classs = "Test";
+        cout << "Vybral si postavu " << classs << "." << endl;
+        acc_stat = 999;
+        dmg_stat = 999;
+        hp_stat = 999;
+        rng_stat = 999;
+        cash = 6742021;
+        break;
+        default :
+    cout << "----------------------------" << endl;
+    cout << endl;
+    cout << endl;
+    cout << "Spatny Input: RESTARTUJ HRU!" << endl;
+    cout << endl;
+    cout << endl;
+    cout << "----------------------------" << endl;
  }
- cout << "mas:" << endl;
- cout << "presnost " << acc_stat << endl;
- cout << "poskozeni " << dmg_stat << endl;
- cout << "zivoty " << hp_stat << endl;
- cout << "stesti " << rng_stat << endl;
- cout << "note: to ze mas zivozy na 3ce neznamena to ze mas 4 zivoty" << endl;
- cout << "prejes si zmenit postavu? (a/n):";
+ cout << "máš:" << endl;
+ cout << "přesnost " << acc_stat << endl;
+ cout << "poškození " << dmg_stat << endl;
+ cout << "životy " << hp_stat << endl;
+ cout << "štěsti " << rng_stat << endl;
+ cout << "note: to že máš životy na 3ce neznamená to že máš 4 životy." << endl;
+ cout << "chceš pokračovat s tvojí postavou? (a/n):";
  do{
     if(opakovat!='a' && opakovat!='n'){
-        cout << endl << "zadejte a nebo n (malym)" << endl;
+        cout << endl << "zadejte a nebo n (malým)" << endl;
     }
 cin >> opakovat;
 }while(opakovat!='a' && opakovat!='n');
-}while(opakovat=='a');
+}while(opakovat=='n');
+cout << "Je rok 1908 (Enter)";
+enter();
+cout << "Jsi majitelem maleho pozemku s domkem pobliíš" << endl << "endl Dubois v Americkém Wayomingu.";
+enter();
+cout << "Za opaskem ti cinká cattleman revolver (0 acc, 0 dmg) po tvém tátovi.";
+enter();
+cout << "Přesně tak, jak to otcové zakladatelé zamýšleli...";
+enter();
+cout << "Deš do duboisu. Už si zapoměl proč, ale je to vlastné jedno.";
+enter();
+                                                                                        //Vesnice
+cout << "Chceš jít do nějákého obchodu?" << endl;
+cout << "   1-Obchod se zbraněmi" << endl;
+cout << "   2-Obchod s oblblečením" << endl;
+cout << "   3-Drugstore" << endl;
+cout << "   4-Jít dál" << endl;
+
+do{
+    vyber=0;
+    if(vyber<0||vyber>4){
+    cout << "Zadejte cisla 1,2,3 nebo 4." << endl;
+    }
+cout << "Vyber:";
+cin>> vyber;
+        }while(vyber<0||vyber>4);
+
+switch (vyber) {                                                                        // zbrane
+    case 1 :
+    do{
+        cout << "Dnešní nabídka:\n";
+        cout << "(Note: Když is koupíš zbraň, zahodíš předchozí)\n";
+        cout << "1. $350 Schofield (1 dmg)\n";
+        cout << "2. $400 Doublebarrel (1 acc)\n";
+
+    do{
+    vyber=0;
+    if(vyber<0||vyber>4){
+    cout << "Zadejte cisla 1,2,3 nebo 4." << endl;
+    }
+cout << "Vyber:";
+cin>> vyber;
+        }while(vyber<0||vyber>4);
+        
+    switch (vyber) {
+        case 1:
+            if (cash>=350){
+                cash-=350;
+                getZbran(1, zbran, zbranDmg, zbranAcc);
+                cout << "+Schofield\n";
+                cout<<"Máš $" << cash << ".\n";
+            }else{
+                cout<<"Nemáš dost peněz! ($" << cash << ")\n";
+            }
+        break;
+        case 2:
+         if (cash>=400){
+                cash-=400;
+                getZbran(2, zbran, zbranDmg, zbranAcc);
+                cout << "+Doublebarrel\n";
+                cout<<"Máš $" << cash << ".\n";
+            }else{
+                cout<<"Nemáš dost peněz! ($" << cash << ")\n";
+            }
+        break;
+
+        default : 
+            cout << "----------------------------\n\n\n";
+            cout << "Spatny Input: RESTARTUJ HRU!\n\n\n";
+            cout << "----------------------------\n";
+        break;
+}
+    cout << "chceš pokračovat? (a/n):";
+ do{
+    if(opakovat!='a' && opakovat!='n'){
+        cout << endl << "zadejte a nebo n (malým)" << endl;
+    }
+cin >> opakovat;
+}while(opakovat!='a' && opakovat!='n');
+}while(opakovat=='n');
+break;
+
+    default : 
+        cout << "----------------------------\n\n\n";
+        cout << "Spatny Input: RESTARTUJ HRU!\n\n\n";
+        cout << "----------------------------\n";
+}
+vyber=0;
 }
